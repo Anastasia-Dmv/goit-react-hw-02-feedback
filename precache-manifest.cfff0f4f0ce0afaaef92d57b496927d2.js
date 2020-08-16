d4f23053220f7403e336576b0ad1d82a88089a4e
Anastasia-Dmv <Vesta50000@gmail.com>
2020-08-16 17:14:39 +0300

    Updates

---
+++ precache-manifest.cfff0f4f0ce0afaaef92d57b496927d2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b606e358e0c1d0e948536b865d66cd4d",
+    "url": "/goit-react-hw-02-feedback/index.html"
+  },
+  {
+    "revision": "0787a3c59375355658f1",
+    "url": "/goit-react-hw-02-feedback/static/js/2.fc3c0e7c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-02-feedback/static/js/2.fc3c0e7c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5f4fb199f4360656953",
+    "url": "/goit-react-hw-02-feedback/static/js/main.20cb30bf.chunk.js"
+  },
+  {
+    "revision": "cb08e2ec47267fd4ee9f",
+    "url": "/goit-react-hw-02-feedback/static/js/runtime-main.9cb7e66e.js"
+  }
+]);
